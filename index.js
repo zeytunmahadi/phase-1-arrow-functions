104b4dcf2e431e209af6f5758856552a6348f98a
zeitun <zeytunmahadi718@gmail.com>
2024-05-18 16:24:00 +0300

    lab on arrow functions

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const divide = function(a, b) {
+    if (b === 0) {
+        throw new Error("Division by zero is not allowed.");
+    }
+    return a / b;
+};
+
+
+const square = (x) => {
+    return x * x;
+};
+
+
+
+const add = (a, b) => {
+    return a + b;
+};
+
+
